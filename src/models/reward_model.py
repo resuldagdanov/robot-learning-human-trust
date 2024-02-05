60a87fa1cd1e34ac890ca801f6f3a66872aea4e6
resuldagdanov <resuldagdanov@gmail.com>
2024-02-05 20:32:02 +1100

    update reward model correctly

--- src/models/reward_model.py
+++ src/models/reward_model.py
@@ -2,25 +2,47 @@ import torch
 
 
 class RewardFunction(torch.nn.Module):
+
     def __init__(self,
-                 state_action_size=7,
-                 hidden_size=64, 
-                 out_size=1,
-                 device="cpu"):
+                 state_action_size: int = 7,
+                 hidden_size: int = 64, 
+                 out_size: int = 1,
+                 device: str = "cpu") -> object:
         
         super(RewardFunction,
               self).__init__()
 
         self.device = device
 
-        self.reward = torch.nn.Sequential(torch.nn.Linear(state_action_size, hidden_size, bias=True),
-                                          torch.nn.ReLU(),
-                                          torch.nn.Linear(hidden_size, hidden_size, bias=True),
-                                          torch.nn.ReLU(),
-                                          torch.nn.Linear(hidden_size, out_size, bias=True),
+        # reward network backbone linear layers
+        self.backbone = torch.nn.Sequential(torch.nn.Linear(state_action_size, hidden_size, bias=True),
+                                            torch.nn.ReLU(),
+                                            torch.nn.Linear(hidden_size, hidden_size, bias=True),
+                                            torch.nn.ReLU())
+        
+        # reward function output being a squashed (with Tanh) to result in a value between -1 and -1
+        self.reward = torch.nn.Sequential(torch.nn.Linear(hidden_size, out_size, bias=True),
                                           torch.nn.Tanh())
         
-    def forward(self, x):
+    def forward(self,
+                x: torch.Tensor) -> torch.Tensor:
+
+        # propagate through reward network backbone
+        x = self.backbone(x)
         value = self.reward(x)
 
         return value
+    
+    def estimate_reward(self,
+                        state_action: torch.Tensor,
+                        is_inference: bool = False) -> torch.Tensor:
+        
+        if is_inference:
+            self.eval()
+            # inference
+            with torch.no_grad():
+                reward_value = self.forward(x=state_action)
+        else:
+            reward_value = self.forward(x=state_action)
+        
+        return reward_value
