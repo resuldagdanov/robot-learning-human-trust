a1e5dcdb346824fa857017f63728429c458fae95
resuldagdanov <resuldagdanov@gmail.com>
2024-02-20 00:28:41 +1100

    example of behavior cloning optimization with gnll

--- src/optimization/train_policy.py
+++ src/optimization/train_policy.py
@@ -1,10 +1,11 @@
 import os
 import sys
 
+import numpy as np
+
 import torch
 
 from tqdm import tqdm
-from sklearn.model_selection import train_test_split
 
 # get the current script's directory
 current_directory = os.path.dirname(os.path.abspath(__file__)) if "__file__" in locals() else os.getcwd()
@@ -13,13 +14,16 @@ parent_directory = os.path.dirname(current_directory)
 # add the parent directory to the sys.path
 sys.path.append(parent_directory)
 
-from utils import constants
+from utils import constants, common
 from utils.dataset_loader import PolicyDatasetLoader
 
+from optimization import functions
 from optimization.updater import Updater
-from optimization.functions import setup_config, get_directories, create_directories, save_policy, read_each_loader
+
+from environment.environment import RobotEnvironment
 
 from models.policy_model import RobotPolicy
+from models.reward_model import RewardFunction
 
 
 if __name__ == "__main__":
@@ -27,117 +31,109 @@ if __name__ == "__main__":
     # available training machine
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
     print("Training Device: ", device)
-
-    # setup hyperparameters
-    configs = setup_config(device=device)
+    
+    configs = functions.setup_config(device=device)
 
     # create and return preliminary base paths
-    json_paths, results_path = get_directories(parent_directory=parent_directory)
-    policy_saving_path, _ = create_directories(configs=configs,
-                                               results_path=results_path,
-                                               saving_policy=True,
-                                               saving_reward=False)
+    json_paths_train, results_path = functions.get_directories(parent_directory=parent_directory,
+                                                               data_folder_name=constants.DEMO_COLLECTION_DATE)
+    policy_saving_path, reward_saving_path = functions.create_directories(configs=configs,
+                                                                          results_path=results_path,
+                                                                          saving_policy=True,
+                                                                          saving_reward=False)
     
-    # load demonstrations dataset
-    all_data = PolicyDatasetLoader(demo_data_json_paths=json_paths)
-
-    # split dataset into training and validation sets
-    train_data, val_data = train_test_split(all_data,
-                                            test_size=configs.validation_split,
-                                            random_state=configs.seed)
-
-    train_loader = torch.utils.data.DataLoader(train_data,
-                                               batch_size=configs.batch_size,
+    # load train dataset of demonstrations
+    training_data = PolicyDatasetLoader(demo_data_json_paths=json_paths_train)
+    train_loader = torch.utils.data.DataLoader(training_data,
+                                               batch_size=configs.batch_size_policy,
                                                shuffle=configs.data_shuffle,
                                                num_workers=configs.num_workers)
-    val_loader = torch.utils.data.DataLoader(val_data,
-                                             batch_size=configs.batch_size,
-                                             shuffle=False,
-                                             num_workers=configs.num_workers)
-    
+
+    # get all indice numbers where the new trajectory is initialized in the dataset
+    trajectory_indices = functions.find_indices_of_trajectory_changes(dataset=training_data)
+
+    # we need both robot policy and reward function models
     policy_network = RobotPolicy(state_size=configs.state_size,
                                  hidden_size=configs.hidden_size,
                                  out_size=configs.action_size,
                                  log_std_min=configs.policy_log_std_min,
                                  log_std_max=configs.policy_log_std_max,
-                                 log_std_init=configs.policy_log_std_init,
                                  device=configs.device)
+    reward_network = RewardFunction(state_size=configs.state_size,
+                                    hidden_size=configs.hidden_size,
+                                    out_size=configs.reward_size,
+                                    device=configs.device)
+    
+    # folder name where reward model parameters are located ("results / reward_network_params / loading_folder_name")
+    reward_loading_folder_name = constants.REWARD_LOADING_FOLDER
+    reward_params_name = constants.REWARD_PARAMS_NAME
 
-    updater = Updater(configs=configs,
-                      policy_network=policy_network,
-                      reward_network=None)
-    updater.initialize_optimizers()
+    # load pretrained reward network parameters if the pre-trained model is available
+    reward_network = functions.load_reward_from_path(reward_network=reward_network,
+                                                     results_path=results_path,
+                                                     reward_loading_folder_name=reward_loading_folder_name,
+                                                     reward_params_name=reward_params_name)
+    # set model to evaluation mode
+    for param in reward_network.parameters():
+        param.requires_grad = False
+    reward_network = reward_network.eval()
+    
+    # model optimizers and learning rate schedulers
+    updater_obj = Updater(configs=configs,
+                          policy_network=policy_network,
+                          reward_network=reward_network)
+    updater_obj.initialize_optimizers()
 
-    # parameters for early stopping criteria
-    best_bc_val_loss = float("inf")
-    early_stopping_counter = 0
+    # construct custom environment while the reward function is in evaluation mode
+    env = RobotEnvironment()
+    env.set_reward_network(reward_network)
+    env.is_reward_inference = True
+
+    loss_policy_list = []
+
+    # currently nu weight is zero; will be updated later
+    nu_factor = torch.tensor(0.0)
 
     print("\n================== Training Initialized (chuff chuff) ==================\n")
 
     for epoch in range(constants.BC_NUMBER_EPOCHS):
 
-        print("================== Training Phase ==================")
-        policy_network.train()
-        cummulative_train_loss = 0.0
+        # compute standar deviation minimization weight based on the annealing strategy
+        std_weight = common.compute_annealing_factor(epoch=epoch,
+                                                     max_epochs=constants.BC_NUMBER_EPOCHS)
 
-        # loop through each batch inside the training dataset
+        # loop through each batch inside the dataset
         for batch_train_data in tqdm(train_loader):
 
             # get batch of data
-            input_state, output_action, _, _ = read_each_loader(configs=configs,
-                                                                sample_data=tuple(batch_train_data))
+            input_state, output_action, _, _ = functions.read_each_loader(configs=configs,
+                                                                          sample_data=tuple(batch_train_data))
             
-            # forward pass to get Gaussian distribution
-            action_pred, action_std, action_log_prob, action_entropy, action_mu_and_std, action_dist = policy_network.estimate_action(state=input_state,
-                                                                                                                                      is_inference=False)
-
-            # multivariate Gaussian negative log-likelihood loss function
-            nll_train_loss = updater.multivariate_gaussian_nll_loss(y_true=output_action,
-                                                                    y_pred=action_mu_and_std)
+            # forward pass to get mean of Gaussian distribution
+            action, action_std, action_log_prob, action_entropy = policy_network.estimate_action(state=input_state,
+                                                                                                 is_policy_inference=False,
+                                                                                                 is_deterministic=True)
             
-            # backward pass and optimization
-            updater.run_policy_optimizer(bc_loss=nll_train_loss)
+            # compute negative log-likelihood loss value for maximum likelihood estimation
+            loss_bc_traj = updater_obj.multivariate_gaussian_nll_loss(action_true=output_action,
+                                                                      action_pred_mu=action,
+                                                                      action_log_std=torch.log(action_std),
+                                                                      std_weight=std_weight)
+            batch_loss = loss_bc_traj.mean().item()
 
-            cummulative_train_loss += nll_train_loss.item()
-        
-        # calculate average training loss in the current epoch
-        avg_bc_train_loss_value = round(cummulative_train_loss / len(train_loader), 5)
-        
-        print("================== Validation Phase ==================")
-        policy_network.eval()
-        cummulative_val_loss = 0.0
+            # backward pass and optimization for the policy model
+            updater_obj.run_policy_optimizer(policy_loss=loss_bc_traj)
+            
+            loss_value_str = str(round(batch_loss, 5)).replace(".", "_")
 
-        # freeze neural network parameters during validation
-        with torch.no_grad():
-            # loop through each batch inside the validation dataset
-            for batch_val_data in tqdm(val_loader):
-                input_state, output_action, _, _ = read_each_loader(configs=configs,
-                                                                    sample_data=tuple(batch_val_data))
-                action_pred, action_std, action_log_prob, action_entropy, action_mu_and_std, action_dist = policy_network.estimate_action(state=input_state,
-                                                                                                                                          is_inference=True)
-                nll_val_loss = updater.multivariate_gaussian_nll_loss(y_true=output_action,
-                                                                      y_pred=action_mu_and_std)
-                cummulative_val_loss += nll_val_loss.item()
-        
-        # calculate average validation loss in the current epoch
-        avg_bc_val_loss_value = round(cummulative_val_loss / len(val_loader), 5)
+        loss_policy_list.append(batch_loss)
 
-        print(f"Epoch {epoch + 1}/{constants.BC_NUMBER_EPOCHS}, Batch Train Loss: {avg_bc_train_loss_value}, Batch Validation Loss: {avg_bc_val_loss_value}")
-        
-        # check for early stopping
-        if avg_bc_val_loss_value < best_bc_val_loss:
-            best_bc_val_loss = avg_bc_val_loss_value
+        print(f"Epoch {epoch + 1}/{constants.BC_NUMBER_EPOCHS}, Batch Loss: {round(batch_loss, 5)}")
 
-            # save action policy network parameters after every epoch
-            save_policy(epoch=epoch,
-                        policy_network=policy_network,
-                        saving_path=policy_saving_path,
-                        loss_value_str=str(avg_bc_train_loss_value).replace(".", "_"))
-        else:
-            early_stopping_counter += 1
-        
-        if early_stopping_counter >= configs.early_stopping_patience:
-            print(f"Early stopping at epoch {epoch + 1} due to no improvement in validation loss!")
-            break
-
+        if epoch % 10 == 0:
+            functions.save_policy(epoch=epoch,
+                                  policy_network=policy_network,
+                                  saving_path=policy_saving_path,
+                                  loss_value_str=str(abs(np.mean(loss_policy_list))).replace(".", "_"))
+    
     print("\n================== Training Finished (choo choo) ==================\n")
